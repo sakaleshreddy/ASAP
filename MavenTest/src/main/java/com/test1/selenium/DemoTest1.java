7289a0304db186e102e64acc06abb771041b3db9
arka2 <arka2@arka2-PC>
2020-09-18 14:20:33 +0530

    Test1 Engineer created this method

---
+++ MavenTest/src/main/java/com/test1/selenium/DemoTest1.java
@@ -0,0 +1,14 @@
+package com.test1.selenium;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+	
+	@Test
+	public void test_01() {
+		Reporter.log("test1 engineer created this method" , true);
+		
+	}
+
+}
